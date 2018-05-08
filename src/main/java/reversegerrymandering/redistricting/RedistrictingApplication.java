a5d02bdb7d17f0560faa2a996453ca33929cfe14
kyeongsoo <followyourinnercompass@gmail.com>
2018-05-07 22:10:05 -0400

    moved from bitbucket

---
+++ src/main/java/reversegerrymandering/redistricting/RedistrictingApplication.java
@@ -0,0 +1,12 @@
+package reversegerrymandering.redistricting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedistrictingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedistrictingApplication.class, args);
+	}
+}
